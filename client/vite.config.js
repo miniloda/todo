28f2c2e39ba510f52566a9ebb6b02197ee09e5dd
jacob <jlusaraga@up.edu.ph>
2024-06-07 02:49:41 +0800

    change vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo",
+  base: "/todo",  // Ensure this is correct for your GitHub Pages setup
+  build: {
+    outDir: 'dist',  // This should match the path in your workflow file
+  },
 })
